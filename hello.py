97a8308b8fd3baf1bd8c3d2c183951f3a8a80f14
msegura069 <MES@MESs-MacBook-Pro.local>
2016-01-16 08:18:05 -0500

    Step 4 of Instructions ("hello world, my name is")

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 # replace the word 'hector' with your own name
 
 def hello_world():
-	return 'hello world, my name is '
+	return 'hello world, my name is Marcial '
 
 print( hello_world() + 'hector' )
